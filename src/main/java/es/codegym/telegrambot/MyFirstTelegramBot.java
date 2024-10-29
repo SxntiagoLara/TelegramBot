7c07be05c7c45c72e9a2d8554ecfa9f70c568c6a
Saaniel <daniel.lara07c@gmail.com>
2024-10-28 18:52:04 -0700

    There are bot for telegram

---
+++ src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
@@ -0,0 +1,26 @@
+package es.codegym.telegrambot;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class MyFirstTelegramBot extends MultiSessionTelegramBot {
+
+    public static final String NAME = "Saaniel_bot";
+    public static final String TOKEN = "8159031271:AAFxRT41bIFPSbyydgQToTUcLuTGB0rsxMw";
+
+    public MyFirstTelegramBot() {
+        super(NAME, TOKEN);
+    }
+
+    @Override
+    public void onUpdateEventReceived(Update update) {
+        sendTextMessageAsync("Hola programador! un gusto conocerte Santiago");
+    }
+
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(new MyFirstTelegramBot());
+    }
+}
